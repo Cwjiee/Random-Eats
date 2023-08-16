08c91f4b2205e7fdfcd4ca18e92bdba75693afe4
Wei Jie <jackchong398@gmail.com>
2023-08-16 23:20:08 +0800

    feat: add layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+
+export default function Navbar(){
+  return (
+    <Box bg="#BEE3F8" w="100%" p={4} color="black" position="fixed" bottom={0}>
+      hello from navbar
+    </Box>
+  )
+}
