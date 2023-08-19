461b059aa537efd646db4dbc137a9a6086b9626c
Wei Jie <jackchong398@gmail.com>
2023-08-19 14:45:31 +0800

    feat: add google auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcs5NP4TT0d8EvkIfEOsu-sHX4m4bY1KU",
@@ -13,4 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,32 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebaseConfig";
+import { useEffect } from "react";
+import router from "next/router";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 export default function Home() {
+  const googleAuth = new GoogleAuthProvider();
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+
+  const googleLogin = async () => {
+    try {
+      await signInWithPopup(auth, googleAuth);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // useEffect(()=> {
+  //   if (user){
+  //     router.push('')
+  //   }
+  // })
+
   return (
     <>
-      
+      <div>home</div>
+      <button onClick={googleLogin}>login</button>
     </>
   )
 }
