e83abeebb49eea3bdd03852d5c55d87f5dca7b62
Cwjiee <weijie@oyencov.com>
2024-05-25 23:27:30 +0800

    feat: qrcode modal on create group

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,20 +10,26 @@ import {
   AbsoluteCenter, 
   Card, 
   CardBody, 
-  Divider, 
   Heading,
   Text,
-  Stack
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  Center,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { apiHandler } from "@/utils/apiHandler";
 import { GoogleAuthProvider } from "firebase/auth";
 import GoogleIcon from "@/components/atoms/GoogleIcon";
 import { v4 } from "uuid";
+import Qrcode from "@/../public/png/qrcode.png"
+import Image from "next/image";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
   const [choices, setChoices] = useState([]);
   const googleAuth = new GoogleAuthProvider()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const items = [
     "hackerspace",
@@ -55,11 +61,25 @@ export default function Home() {
       }
     }, [user]);
 
-   return user ? (
+   return !user ? (
     <>
+      <Modal isOpen={isOpen} placement="bottom" isCentered onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent p={5} w="80%">
+          <Center flexDirection={"column"} gap={5}>
+            <Text as={"b"}>Group up with your friends!</Text>
+            <Image src={Qrcode} />
+            <Flex w={"full"} h={"full"} direction={"column"} justifyContent={"center"} gap={2}>
+              <Button mx={"auto"} colorScheme="twitter" w={"90%"}>Next</Button>
+              <Button mx={"auto"} colorScheme="twitter" w={"90%"} variant={"outline"}>Copy Link</Button>
+            </Flex>
+          </Center>
+        </ModalContent>
+      </Modal>
+
       <Box w={"full"} h={"full"} flexDirection={"column"} mt={"5"}>
         <Flex w={"full"} h={"full"} justifyContent={"center"}>
-          <Button mx={"auto"} colorScheme="twitter" w={"60%"}>Create Group</Button>
+          <Button mx={"auto"} colorScheme="twitter" w={"60%"} onClick={onOpen}>Create Group</Button>
         </Flex>
         <Heading mb={"10"} mt={"16"} textAlign={"left"} size={"md"}>History</Heading>
         {items.map((item) => {
